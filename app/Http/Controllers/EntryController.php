f6f34c41d103c0587d094897a431114ca598481e
Ricardo Gauto <ricardogauto11@gmail.com>
2020-09-26 18:18:26 -0300

    Crud creado

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Entry;
 
 class EntryController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('entries.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|min:7|max:255|unique:entries',
+            'content' => 'required|min:25|max:3000'
+        ]);
+
+        $entry = new Entry();
+        $entry->title = $validatedData['title'];
+        $entry->content = $validatedData['content'];
+        $entry->user_id = auth()->id();
+        $entry->save();
+
+        $status = 'Your entry has been published successfully';
+
+        return back()->with(compact('status'));
+    }
 }
