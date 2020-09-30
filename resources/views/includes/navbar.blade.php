557693a2c9e2744dfd6e44dff7cceb993920c22f
Ricardo Gauto <ricardogauto11@gmail.com>
2020-09-30 09:06:28 -0300

    Enlace a entradas del autor

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -13,6 +13,9 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ url('entries/create') }}"> New entry </a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/home') }}"> My entries </a>
+        </li>
         <li class="nav-item dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                 {{ Auth::user()->name }}
