c097a71c7caf7507c94978b97f1baf4d434a6b6c
Ricardo Gauto <ricardogauto11@gmail.com>
2020-09-30 08:38:50 -0300

    Paginación agregada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>Last Entries</h1>
+            <h1 class="mb-4">Last Entries</h1>
             @foreach ($entries as $entry)
-            <div class="card mt-4">
-                <div class="card-header">{{ $entry->title }}</div>
+            <div class="card mb-4">
+                <div class="card-header">
+                    {{ $entry->title }}
+                </div>
                 <div class="card-body">
                     <p>{{ $entry->content }}</p>
                 </div>
-                <div class="card-footer">{{ $entry->user_id }}</div>
+                <div class="card-footer">
+                     Author: {{ $entry->user_id }}
+                </div>
             </div>
             @endforeach
+            {{ $entries->links() }}
         </div>
     </div>
 </div>
