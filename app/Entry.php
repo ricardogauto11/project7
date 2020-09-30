a81563e4cc66c0e296f581b2aeb970c6b2a3cfbd
Ricardo Gauto <ricardogauto11@gmail.com>
2020-09-30 08:53:13 -0300

    Relacion entradas y usuarios

--- app/Entry.php
+++ app/Entry.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entry extends Model
 {
-    //
+    // Entry N - 1 User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
