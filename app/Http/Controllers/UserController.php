b4d96a3647b82a81c692fc4b508d6825c6359f96
Ricardo Gauto <ricardogauto11@gmail.com>
2020-10-01 09:39:50 -0300

    Obteniendo entradas de cada usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Entry;
 
 class UserController extends Controller
 {
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $entries = Entry::where('user_id', $user->id)->get();
+
+        return view('users.show', compact('user', 'entries'));
     }
 }
