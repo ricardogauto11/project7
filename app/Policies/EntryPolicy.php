7829145121338370187eb8b4c618bc82978db7bc
Ricardo Gauto <ricardogauto11@gmail.com>
2020-10-02 09:36:10 -0300

    Autorizando acceso a editar entradas con Policies

---
+++ app/Policies/EntryPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Entry;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EntryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function update(User $user, Entry $entry)
+    {
+        return $user->id === $entry->user_id;
+    }
+}
